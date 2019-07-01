8762f2992175f4a0788dba27d18a28f4b4885c53
yangguang <yangguang@meican.com>
2019-07-01 22:35:58 +0800

    add s3 pri

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-#/usr/bin/env python
+# /usr/bin/env python
 # coding=utf8
 
-from collections import namedtuple
-from random import choice
+from io import BytesIO
 
-from flask import Flask, jsonify
+import boto3
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
 
-Quote = namedtuple("Quote", ("text", "author"))
+S3_BUCKET = "my-aws-two"
+S3_KEY_PREFIX = "test/{}"
+MYSQL_USER_NAME = "zhimaaread"
+MYSQL_PASSWORD = "zhimaa"
+MYSQL_HOST = "fanslave4.c6cm6m48rq3l.rds.cn-north-1.amazonaws.com.cn"
+MYSQL_DATABASE = "fan"
 
-quotes = [
-    Quote("Talk is cheap. Show me the code.", "Linus Torvalds"),
-    Quote("Programs must be written for people to read, and only incidentally for machines to execute.", "Harold Abelson"),
-    Quote("Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
-          "John Woods"),
-    Quote("Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime.", "Muhammad Waseem"),
-    Quote("Progress is possible only if we train ourselves to think about programs without thinking of them as pieces of executable code. ",
-          "Edsger W. Dijkstra")
-]
+
+def get_connection():
+    engine = create_engine(f"mysql://{MYSQL_USER_NAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}",
+                           connect_args={"encoding": "utf8"})
+    return engine.connect()
+
+
+def result_json(code, desc, data):
+    return {
+        "resultCode": code,
+        "resultDescription": desc,
+        "data": data
+    }
+
 
 app = Flask(__name__)
 
 
-@app.route("/quote", methods=["GET"])
-def get_random_quote():
-    return jsonify(choice(quotes)._asdict())
+@app.route("/to-production", methods=["POST"])
+def to_production():
+    content = request.json
+    file_name = content.get("fileName", "")
+    if file_name == "":
+        return jsonify(result_json("ParamError", "fileName error", ""))
+    f = BytesIO()
+    s3 = boto3.client('s3')
+    s3.download_fileobj(S3_BUCKET, S3_KEY_PREFIX.format(file_name), f)
+    res = f.getvalue().decode('utf8').strip()
+    return jsonify(res)
